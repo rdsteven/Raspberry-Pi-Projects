50bf796885662df9aa8e76a8096135eb49e2cce5
Jack Smith <sythasfaroth@gmail.com>
2016-12-20 21:21:51 -0600

    added basic distance track and motor coord

--- car/python/server/hug_wall.py
+++ car/python/server/hug_wall.py
@@ -2,15 +2,58 @@
 import RPi.GPIO as GPIO
 import car_dir
 import motor
-import time   
+import time
 
-car_dir.setup()
-motor.setup()
-car_dir.home()
-motor.setSpeed(100)
+TRIG = 11
+ECHO = 12
+
+def setupPing():
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(TRIG, GPIO.OUT)
+	GPIO.setup(ECHO, GPIO.IN)
+
+def distance():
+	GPIO.output(TRIG, 0)
+	time.sleep(0.000002)
+
+	GPIO.output(TRIG, 1)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, 0)
+
+	while GPIO.input(ECHO) == 0:
+		a = 0
+	time1 = time.time()
+	while GPIO.input(ECHO) == 1:
+		a = 1
+	time2 = time.time()
+
+	during = time2 - time1
+	return during * 340 / 2 * 100
+
+def init():
+    car_dir.setup()
+    motor.setup()
+    car_dir.home()
+    setupPing()
+    motor.setSpeed(50)
+
+init()
 motor.forward()
-time.sleep(3)
+i = 0
+while (i < 4):
+    d = distance()
+    print d
+
+    if (d < 60):
+        i = i + 1
+    else:
+        i = 0
+
 motor.stop()
+
+#motor.forward()
+#time.sleep(3)
+#motor.stop()
 
 #while True:
 #			motor.forward()
@@ -41,4 +84,3 @@ motor.stop()
 #			try:
 #				spd = int(spd)
 #	                        motor.backward(spd)
-
