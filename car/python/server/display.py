71f04363d76717016f265863647a20aa0a301562
Jack Smith <sythasfaroth@gmail.com>
2017-01-17 20:55:13 -0600

    added message func

--- car/python/server/display.py
+++ car/python/server/display.py
@@ -6,7 +6,10 @@ class Display:
     def __init__(self):
         LCD1602.init(0x27, 1)
 
-    def displayIP():
+    def displayIP(self):
         iface = netifaces.ifaddresses('wlan0')
-        LCD1602.write(0, 0, "IP: " + iface[netifaces.AF_INET][0]['addr'])
+        self.message("IP: " + iface[netifaces.AF_INET][0]['addr'])
         # iwgetid -r to get network name
+
+    def message(msg):
+        LCD1602.write(0, 0, msg)
